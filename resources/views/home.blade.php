e407e74f16a297e02c99d496a8c3765287996738
gabrielleabasolo <gabrielleabasolo@gmail.com>
2023-06-09 22:15:49 +0800

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-template>
+    <div class="container mx-auto">
+        <div class="border-l-fuchsia-300 border-black">
+            <h1 class="text-9xl text-center">HOME</h1>
+        </div>
+    </div>
+</x-template>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Finance Tracker</title>
+</head>
+<body>
+    <div class="flex">
+        <div class="flex-1">
+           <p >home</p>
+        </div>
+        <div>
+            <p class="mr-5">login</p>
+            <p class="mr-5">Register</p>
+        </div>   
+    </div>
+</body>
+</html>
